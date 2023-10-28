97cda2f0e4b1e8f9cc9ed3dd62bde85ae99ff520
gifft-sequoiaat <gift@sequoiaat.com>
2023-10-28 15:39:43 +0530

    chore: made it as a immediate invoked function

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-window.addEventListener("message", function (event) { 
-   console.log("i got execute")
-            if (event.origin !== "http://localhost:5173") return;
-            if(event.data === "showChatbot") {
-              const element =  document.getElementById('chatena-iframe')
-               element.style.height = "600px";
-               element.style.width = "400px";
-            } else {
-               const element =  document.getElementById('chatena-iframe')
-               element.style.height = "64px";
-               element.style.width = "64px";
-            }
-}); 
+(function () {
+   window.addEventListener("message", function (event) {
+       if (event.origin !== "http://localhost:5173") return;
+       if (event.data === "showChatbot") {
+           const element = document.getElementById('chatena-iframe');
+           element.style.height = "600px";
+           element.style.width = "400px";
+       } else {
+           const element = document.getElementById('chatena-iframe');
+           element.style.height = "64px";
+           element.style.width = "64px";
+       }
+   });
+})();
